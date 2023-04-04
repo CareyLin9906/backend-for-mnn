feda276a7275b57a3c16ebc5efc5f855db61f0c6
MrGuess024 <107774690+MrGuess024@users.noreply.github.com>
2023-04-04 13:21:09 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,22 @@
 import argparse
 import sys
 
-dataset = sys.argv[1]
+print(str(sys.argv[1]));
 
-print(dataset)
-// print(str(sys.argv[1]));
+
+# dataset = sys.argv[1]
 
 # Choose dataset based on argument
-def choose(dataset):
-    if dataset == "cat":
-        print("cat")
-    elif dataset == "dog":
-        print("dog")
-    elif dataset =="ship":
-        print("ship")
-    elif dataset =="car":
-        print("car")
-    else:
-        print(f"Unknown dataset: {dataset}")
+# def choose(dataset):
+#    if dataset == "cat":
+#        print("cat")
+#    elif dataset == "dog":
+#        print("dog")
+#    elif dataset =="ship":
+#        print("ship")
+#    elif dataset =="car":
+#        print("car")
+#   else:
+#        print(f"Unknown dataset: {dataset}")
 
 choose(dataset)
