8bf6531b6db8edcd443718331b72b84b8f1cd9ec
Carey Lin <lin1433@purdue.edu>
2023-04-19 03:42:52 +0800

    minor fix

--- script.py
+++ script.py
@@ -4,11 +4,7 @@ import grouper
 
 
 data = sys.argv[1]
-"""
-dummy variables:
-class number = 5
-path of average softmax matrix: root_avgsoftmax.pth
-"""
+
 grouping_volatility = sys.argv[2]
 avgsoftmax = "root_avgsoftmax.pth"
 classNumber = 5
